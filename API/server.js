2578b188b803b28400cdee5bd6567bbb57a40f6b
Carmine Calabrese <carmine.calabrese99@gmail.com>
2024-06-04 20:07:06 +0200

    Added method for the drivers

--- API/server.js
+++ API/server.js
@@ -28,14 +28,42 @@ app.listen(3000, () => {
 	console.log("Server running on port 3000");
 });
 
-//API METHODS
-app.get("/api/drivers/GetDriverData", (req, res) => {
+/*
+	API METHODS
+	 TODO: Driver Methods (getAll , getById)
+	 TODO: Team Methods (getAll , getById)
+	 TODO: Circuit Methods (getAll , getById)
+	 TODO: Race Methods (getAll , getById)
+	 TODO: Season Methods (getAll)
+
+	 ! IMPORTANT: Trovare collegamenti fra i vari dataset per rendere complete le pagine (Basarsi sugli ID)
+*/
+
+//Methods to get all the drivers in the database
+app.get("/api/drivers/GetAllDrivers", (req, res) => {
 	database
 		.collection("drivers")
 		.find({})
 		.toArray((error, result) => {
 			if (!error) {
 				res.send(result);
+			}
+		});
+});
+
+app.get("/api/drivers/GetDriverById/:id", (req, res) => {
+	console.log("GetDriverbyId:", req.params.id);
+	if (req.params.id == null) {
+		res.send("No ID specified");
+		return;
+	}
+	database
+		.collection("drivers")
+		.find({ driverId: parseInt(req.params.id) })
+		.toArray((error, result) => {
+			if (!error) {
+				res.send(result);
+				console.log(result);
 			}
 		});
 });
